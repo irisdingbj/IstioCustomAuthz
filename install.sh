a0614a79eb604feb62d28bebcfde9955313b25eb
Chen-Xintong <xintong.chen@intel.com>
2022-04-25 15:51:32 +0800

    update hosts

--- install.sh
+++ install.sh
@@ -1,13 +1,19 @@
 kubectl create ns foo
 kubectl label ns foo istio-injection=enabled
-kubectl apply -f istio-1.13.2/samples/httpbin/httpbin.yaml -n foo
-kubectl apply -f istio-1.13.2/samples/sleep/sleep.yaml -n foo
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.13/samples/httpbin/httpbin.yaml -n foo
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.13/samples/sleep/sleep.yaml -n foo
 
 kubectl exec "$(kubectl get pod -l app=sleep -n foo -o jsonpath={.items..metadata.name})" -c sleep -n foo -- curl http://httpbin.foo:8000/ip -s -o /dev/null -w "%{http_code}\n"
 
 kubectl apply -f ext-authz-oauth2-proxy.yaml 
 
+
+kubectl edit configmap istio -n istio-system
+
 kubectl apply -f istio-configmap.yaml 
+kubectl rollout restart deployment/istiod -n istio-system
+
+kubectl apply -f httpbin-istio-gateway.yaml
 
 kubectl apply -f gateway-virtualservice.yaml
 
@@ -20,3 +26,10 @@ helm install \
 
 # keycloak svc
 # keycloak.keycloak.svc.cluster.local:30802
+
+kubectl exec "$(kubectl get pod -l app=sleep -n foo -o jsonpath={.items..metadata.name})" -c sleep -n foo -- curl "http://httpbin.foo:8000/headers" -H "authorization: allow" -s
+curl "http://httpbin.foo:8000/headers" -H "authorization: allow" -s
+
+kubectl exec "$(kubectl get pod -l app=sleep -n foo -o jsonpath={.items..metadata.name})" -c sleep -n foo -- curl http://httpbin.foo:8000/ip -s -o /dev/null -w "%{http_code}\n"
+curl http://httpbin.foo:8000/ip -H "authorization: allow" -s -o /dev/null -w "%{http_code}\n"
+x-forwarded-access-token
